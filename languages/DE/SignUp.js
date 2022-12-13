fa1980214a9d394254c8ec0d228a9e801067d180
Artyom Kolesen <artyomkolesen@gmail.com>
2022-12-13 16:30:01 +0300

    Add timeout to success toadter; change signp text

--- languages/DE/SignUp.js
+++ languages/DE/SignUp.js
@@ -2,7 +2,7 @@ export default {
 	signUp: 'ALS NEUKUNDE BUCHEN',
 	password: 'Wir senden Ihnen ein Passwort an Ihre E-Mail-Adresse',
 	youHaveRegistered: 'Sie haben sich erfolgreich registriert!',
-	checkEmail: email => `Überprüfen Sie bitte Ihre E-Mail: ${email}`,
+	checkEmail: email => `Wir haben die Anmeldeparameter dupliziert zu Ihrer E-Mail (${email})`,
 	passportPhoto: 'Passfoto (letzte Seite)',
 	selfieWithPassport: 'Ihr Selfie mit der letzten Seite des Reisepasses',
 	legalDocumentPhoto: 'Foto der legalen Einreiseregistrierung'
